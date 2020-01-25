0dde5e2efbdcaf6f510e610edb5d64e941a6a61b
Chris Stevenson <cj.stevenson@live.com>
2020-01-24 22:28:03 -0600

    doing a bit of file reading

--- tools/item-validator/src/main.rs
+++ tools/item-validator/src/main.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io::Read;
+use std::str;
 
 mod item_model;
 
@@ -5,15 +8,21 @@ pub use crate::item_model::*;
 
 fn main() {
 
-    let file_path = "../../../data/items";
+    let mut file = File::open("../../../data/items/1-explosive-uni.yaml").expect("can't find file");
+    
 
-    let x = Item{
-        name: "test".to_string(),
-        item_number: 1,
-        level: 1,
-        category_list: vec!["".to_string()],
-        elements : vec![ElementValue{element:Element::Fire, element_value:1}]
-    };
+    let mut buffer = Vec::new();
+    // read the whole file
+    file.read_to_end(&mut buffer).expect("unable to read from file");
 
-    println!("{:#?}",x);
+    let item_text = str::from_utf8(&buffer).expect("file is not valid utf8");
+    // let x = Item{
+    //     name: "test".to_string(),
+    //     item_number: 1,
+    //     level: 1,
+    //     category_list: vec!["".to_string()],
+    //     elements : vec![ElementValue{element:Element::Fire, element_value:1}]
+    // };
+
+    println!("{}",item_text);
 }
