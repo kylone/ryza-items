a717c64b1396e716a78b2b125682661a02c93da4
Chris Stevenson <cj.stevenson@live.com>
2020-01-25 21:55:14 -0600

    readinf files in items folder

--- tools/item-validator/src/main.rs
+++ tools/item-validator/src/main.rs
@@ -11,8 +11,21 @@ pub use crate::item_model::*;
 
 fn get_item_files(path: &Path,file_buffers: &mut Vec<Vec<u8>>) -> io::Result<()> {
     let files = fs::read_dir(path)?;
-    for file in files{
-            println!("{:#?}", file)
+    for file_result in files{
+        let file = file_result?;
+        let file_name = file.file_name();
+        println!("opening {:?}", file_name);
+        let mut file = File::open(file.path())?;
+        
+        let mut buffer = Vec::new();
+        // read the whole file
+        file.read_to_end(&mut buffer)?;
+        let item_text = str::from_utf8(&buffer).expect("file is not valid utf8");
+
+        println!("{}",item_text);
+        println!("   -----     ");
+        // done using specific buffer, store in file buffers for reference
+        file_buffers.push(buffer)
     }
     Ok(())
 }
@@ -22,14 +35,15 @@ fn main() {
     let path_for_item_files = Path::new("../../../data/items/");
 
     get_item_files(&path_for_item_files, &mut file_buffers).unwrap();
-    let mut file = File::open("../../../data/items/1-explosive-uni.yaml").expect("can't find file");
     
 
-    let mut buffer = Vec::new();
-    // read the whole file
-    file.read_to_end(&mut buffer).expect("unable to read from file");
+    // let mut buffer = Vec::new();
+    // // read the whole file
+    // file.read_to_end(&mut buffer).expect("unable to read from file");
 
-    let item_text = str::from_utf8(&buffer).expect("file is not valid utf8");
+    // let item_text = str::from_utf8(&buffer).expect("file is not valid utf8");
+    // println!("{}",item_text);
+
     // let x = Item{
     //     name: "test".to_string(),
     //     item_number: 1,
@@ -38,5 +52,4 @@ fn main() {
     //     elements : vec![ElementValue{element:Element::Fire, element_value:1}]
     // };
 
-    println!("{}",item_text);
 }
