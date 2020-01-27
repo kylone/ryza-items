8f9f6bb7f63117851f79f38387440c717a399f0f
Chris Stevenson <cj.stevenson@live.com>
2020-01-26 20:47:31 -0600

    validation of categories implemented

--- tools/item-validator/src/main.rs
+++ tools/item-validator/src/main.rs
@@ -1,18 +1,34 @@
 mod file_contents;
 mod validate_item;
 
-fn main() {
-    let item_lists =
+
+fn get_item_validation_sets() -> validate_item::ItemValidationSets{
+    let item_list_contents =
         file_contents::load_lists_file("../../../data/lists.yml").expect("can't load lists.yml");
 
-    println!("{}", item_lists);
+    let item_validation_sets = validate_item::build_item_validation_sets(&item_list_contents).expect("can't parse item validation lists");
+
+    println!("Parsed lists.yml:");
+    println!("  Found {} Item Categories", item_validation_sets.categories.len());
+    println!("  Found {} Item Classifications", item_validation_sets.classifications.len());
+    println!("  Found {} Elements", item_validation_sets.elements.len());
+    println!("  Found {} Gathering Tools", item_validation_sets.gathering_tools.len());
+    println!("");
+
+    item_validation_sets
+}
+
+fn main() {
+
+    let item_validation_sets = get_item_validation_sets();
+
     let mut item_contents: Vec<file_contents::FileContents> = Vec::new();
     file_contents::load_item_files(&mut item_contents, "../../../data/items/").unwrap();
 
     for file in item_contents {
         println!("Validating {}", file.name);
-
-        let result = validate_item::validate_item_contents(&file.contents);
+        let only_output_invalid = true;
+        let result = validate_item::validate_item_contents(&file.contents, &item_validation_sets, only_output_invalid);
         if result.is_err() {
             println!("unable to validate {}", file.name)
         }
--- tools/item-validator/src/validate_item.rs
+++ tools/item-validator/src/validate_item.rs
@@ -1,9 +1,57 @@
 extern crate yaml_rust;
+use std::collections::HashSet;
 use yaml_rust::{ScanError, Yaml, YamlLoader};
 
 extern crate term;
 
-fn validate_yaml_key(yaml: &Yaml, key: &str) {
+
+pub struct ItemValidationSets {
+    pub elements: HashSet<String>,
+    pub categories: HashSet<String>,
+    pub classifications: HashSet<String>,
+    pub gathering_tools: HashSet<String>,
+}
+
+impl ItemValidationSets {
+    pub fn new() -> ItemValidationSets {
+        ItemValidationSets{
+            elements: HashSet::new(),
+            categories: HashSet::new(),
+            classifications: HashSet::new(),
+            gathering_tools: HashSet::new(),
+        
+        }
+    }
+}
+
+
+
+pub fn build_item_validation_sets(contents: &str) ->Result<ItemValidationSets, ScanError> {
+    let docs = YamlLoader::load_from_str(contents)?;
+    // YAML files can actually contain multiple files inside, we want the first one
+    let doc = &docs[0];
+    let mut validation_sets = ItemValidationSets::new();
+    add_to_set(&doc, "Item Categories", &mut validation_sets.categories);
+    add_to_set(&doc, "Item Classifications", &mut validation_sets.classifications);
+    add_to_set(&doc, "Elements", &mut validation_sets.elements);
+    add_to_set(&doc, "Gathering Tools", &mut validation_sets.gathering_tools);
+    Ok(validation_sets)
+}
+
+fn add_to_set(yaml: &Yaml, key: &str, set: &mut HashSet<String>) {
+    let value = &yaml[key];
+    
+    if let Some(list) = value.as_vec(){
+        for value in list{
+            if let Some (value) = value.as_str() {
+                set.insert(value.to_string());
+            }
+        }
+    } 
+    set.shrink_to_fit();
+}
+
+fn validate_key_exists(yaml: &Yaml, key: &str, only_output_invalid:bool) -> bool {
     let value = &yaml[key];
 
     // there's a whole bunch of .unwrap() calls with the term crate
@@ -15,29 +63,64 @@ fn validate_yaml_key(yaml: &Yaml, key: &str) {
         terminal.fg(term::color::BRIGHT_RED).unwrap();
         println!("'{}' key is missing", key);
         terminal.reset().unwrap();
+        false  // validation unsuccessful
     } else {
-        terminal.fg(term::color::BRIGHT_GREEN).unwrap();
+        if only_output_invalid == false {
+            terminal.fg(term::color::BRIGHT_GREEN).unwrap();
 
-        print!("  {} is present", key);
-        terminal.reset().unwrap();
+            print!("  {} is present", key);
+            terminal.reset().unwrap();
 
-        if let Some(value) = value.as_str() {
-            println!(": {}", value)
-        } else if let Some(value) = value.as_i64() {
-            println!(": {}", value)
-        } else {
-            println!("");
+            if let Some(value) = value.as_str() {
+                println!(": {}", value)
+            } else if let Some(value) = value.as_i64() {
+                println!(": {}", value)
+            } else {
+                println!("");
+            }
         }
+
+        true // validation successful
     }
 }
 
-pub fn validate_item_contents(contents: &str) -> Result<(), ScanError> {
+fn validate_list_values(yaml: &Yaml, validation_set: &HashSet<String>, key: &str, only_output_invalid:bool) -> bool{
+    let value = &yaml[key];
+    let mut terminal = term::stdout().unwrap();
+
+    if let Some(list) = value.as_vec() {
+        let mut all_values_valid = true;
+        terminal.fg(term::color::BRIGHT_RED).unwrap();
+        for value in list{
+            if let Some(value) = value.as_str() {
+                if validation_set.contains(value) == false{
+                    all_values_valid = false;
+                    println!("  {}: {} is not a valid value", key, value);
+                }
+            }
+        }
+        terminal.reset().unwrap();
+        if all_values_valid && only_output_invalid == false{
+            terminal.fg(term::color::BRIGHT_GREEN).unwrap();
+            println!("  {} values are valid", key);
+            terminal.reset().unwrap();
+        }
+        true
+    } 
+    else {
+        false // not a yaml list (a vector)
+    }
+}
+
+pub fn validate_item_contents(contents: &str, item_validation_sets: &ItemValidationSets, only_output_invalid:bool) -> Result<(), ScanError> {
     let docs = YamlLoader::load_from_str(contents)?;
     // YAML files can actually contain multiple files inside, we want the first one
     let doc = &docs[0];
-    validate_yaml_key(&doc, "Name");
-    validate_yaml_key(&doc, "Item Number");
-    validate_yaml_key(&doc, "Level");
-    validate_yaml_key(&doc, "Category");
+    validate_key_exists(&doc, "Name", only_output_invalid);
+    validate_key_exists(&doc, "Item Number", only_output_invalid);
+    validate_key_exists(&doc, "Level", only_output_invalid);
+    if validate_key_exists(&doc, "Category", only_output_invalid){
+        validate_list_values(&doc, &item_validation_sets.categories, "Category", only_output_invalid);
+    }
     Ok(())
 }
